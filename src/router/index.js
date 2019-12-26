ffe7626d172a48dcc3fbe9f8f9788880f7c82d29
fkrugerr <fkalinin@gmail.com>
2019-12-26 12:29:08 +0200

    Initial config

--- src/router/index.js
+++ src/router/index.js
@@ -11,12 +11,14 @@ const routes = [
     component: Home,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/desktop',
+    name: 'desktop',
+    component: () => import(/* webpackChunkName: "desktop" */ '../views/Desktop.vue'),
+  },
+  {
+    path: '/transactions',
+    name: 'transactions',
+    component: () => import(/* webpackChunkName: "transactions" */ '../views/Transactions.vue'),
   },
 ];
 
---
+++ src/views/Transactions.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>desctop</h1>
+  </div>
+</template>
